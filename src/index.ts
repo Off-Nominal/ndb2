bd77294feabcff9100846d82ecde66250bee8476
Jake Robins <me@jakerobins.com>
2023-03-05 16:23:57 -0600

    add port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 80;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Success!!!1" });
+});
 
 app.listen(PORT, () => {
   console.log(`App Listening on Port: `, PORT);
