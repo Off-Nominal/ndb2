9c38736abba817fd2d6faeba96cadb7e5e3264a7
Jake Robins <me@jakerobins.com>
2023-03-13 07:24:12 -0600

    update env var for discord client

--- src/middleware/authenticateApplication.ts
+++ src/middleware/authenticateApplication.ts
@@ -2,7 +2,7 @@ import { APIResponse } from "../types/responses";
 
 export const authenticateApplication = (req, res, next) => {
   const authId = req.get("authorization");
-  const validID = process.env.APP_AUTH_ID;
+  const validID = process.env.DISCORD_CLIENT_API_KEY;
   if (!authId || authId !== `Bearer ${validID}`) {
     const error: APIResponse = {
       success: false,
