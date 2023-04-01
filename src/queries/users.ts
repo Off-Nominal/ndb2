5237eeda3171e2a408bcbbd83bbe693ceb2c6231
Jake Robins <me@jakerobins.com>
2023-04-01 16:22:49 +0100

    add more queries for scores

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -119,33 +119,33 @@ const CREATE_USERS_BETS_SUMMARY = `
       u.id,
       RANK () OVER (
         ORDER BY 
-          (SELECT 
-            COUNT(eb.bet_id)::INT
-          FROM enhanced_bets eb
-          WHERE ((eb.status = 'successful' AND eb.endorsed IS TRUE) OR (eb.status = 'failed' AND eb.endorsed IS FALSE)) AND eb.better_id = u.id) DESC,
-          (SELECT 
-            COUNT(eb.bet_id)::INT
-          FROM enhanced_bets eb
-          WHERE ((eb.status = 'successful' AND eb.endorsed IS FALSE) OR (eb.status = 'failed' AND eb.endorsed IS TRUE)) AND eb.better_id = u.id) ASC
+          COUNT(eb.bet_id) FILTER 
+            (WHERE 
+              (eb.status = 'successful' AND eb.endorsed IS TRUE) OR
+              (eb.status = 'failed' AND eb.endorsed IS FALSE)
+            )::INT DESC,
+          COUNT(eb.bet_id) FILTER 
+            (WHERE 
+              (eb.status = 'successful' AND eb.endorsed IS FALSE) OR
+              (eb.status = 'failed' AND eb.endorsed IS TRUE)
+            )::INT ASC
       ) as rank,
-      (SELECT 
-          COUNT(eb.bet_id)::INT
-        FROM enhanced_bets eb
-        WHERE ((eb.status = 'successful' AND eb.endorsed IS TRUE) OR (eb.status = 'failed' AND eb.endorsed IS FALSE)) AND eb.better_id = u.id) as successful,
-    (SELECT 
-          COUNT(eb.bet_id)::INT
-        FROM enhanced_bets eb
-        WHERE ((eb.status = 'successful' AND eb.endorsed IS FALSE) OR (eb.status = 'failed' AND eb.endorsed IS TRUE)) AND eb.better_id = u.id) as failed,
-    (SELECT 
-          COUNT(eb.bet_id)::INT
-        FROM enhanced_bets eb
-        WHERE (eb.status = 'open' OR eb.status = 'closed') 
-        AND eb.better_id = u.id) as pending,
-    (SELECT 
-          COUNT(eb.bet_id)::INT
-        FROM enhanced_bets eb
-        WHERE eb.status = 'retired' AND eb.better_id = u.id) as retired
+      COUNT(eb.bet_id) FILTER 
+        (WHERE 
+          (eb.status = 'successful' AND eb.endorsed IS TRUE) OR
+          (eb.status = 'failed' AND eb.endorsed IS FALSE)
+        )::INT as successful,
+      COUNT(eb.bet_id) FILTER 
+        (WHERE 
+          (eb.status = 'successful' AND eb.endorsed IS FALSE) OR
+          (eb.status = 'failed' AND eb.endorsed IS TRUE)
+        )::INT as failed,
+      COUNT(eb.bet_id) FILTER 
+        (WHERE eb.status = 'open' OR eb.status = 'closed')::INT as pending,
+      COUNT(eb.bet_id) FILTER (WHERE eb.status = 'retired')::INT as retired
     FROM users u
+    LEFT JOIN enhanced_bets eb ON eb.better_id = u.id
+    GROUP BY u.id
 `;
 
 const CREATE_USERS_PREDICTIONS_SUMMARY = `
@@ -154,43 +154,86 @@ const CREATE_USERS_PREDICTIONS_SUMMARY = `
       u.id,
       RANK () OVER (
         ORDER BY 
-          (SELECT 
-            COUNT(ep.prediction_id)::INT
-          FROM enhanced_predictions ep
-          WHERE ep.status = 'successful' AND ep.predictor_id = u.id) DESC,
-          (SELECT 
-            COUNT(DISTINCT ep.prediction_id)::INT
-          FROM enhanced_predictions ep
-          WHERE ep.status = 'failed' AND ep.predictor_id = u.id) ASC
+          COUNT(DISTINCT ep.prediction_id) FILTER (WHERE ep.status = 'successful')::INT DESC,
+          COUNT(DISTINCT ep.prediction_id) FILTER (WHERE ep.status = 'failed')::INT ASC
       ) as rank,
-      (SELECT 
-          COUNT(DISTINCT ep.prediction_id)::INT
-        FROM enhanced_predictions ep
-        WHERE ep.status = 'successful' AND ep.predictor_id = u.id) as successful,
-    (SELECT 
-          COUNT(DISTINCT ep.prediction_id)::INT
-        FROM enhanced_predictions ep
-        WHERE ep.status = 'failed' AND ep.predictor_id = u.id) as failed,
-    (SELECT 
-          COUNT(DISTINCT ep.prediction_id)::INT
-        FROM enhanced_predictions ep
-        WHERE (ep.status = 'open' OR ep.status = 'closed') 
-        AND ep.predictor_id = u.id) as pending,
-    (SELECT 
-          COUNT(DISTINCT ep.prediction_id)::INT
-        FROM enhanced_predictions ep
-        WHERE ep.status = 'retired' AND ep.predictor_id = u.id) as retired
+      COUNT(DISTINCT ep.prediction_id) FILTER (WHERE ep.status = 'successful')::INT
+        as successful,
+      COUNT(DISTINCT ep.prediction_id) FILTER (WHERE ep.status = 'failed')::INT
+        as failed,
+      COUNT(DISTINCT ep.prediction_id) FILTER (WHERE ep.status = 'open' OR ep.status = 'closed')::INT
+        as pending,
+      COUNT(DISTINCT ep.prediction_id) FILTER (WHERE ep.status = 'retired')::INT
+        as retired
     FROM users u
+    LEFT JOIN enhanced_predictions ep ON ep.predictor_id = u.id
+    GROUP BY u.id
+`;
+
+const CREATE_BET_PAYOUT_TABLE = `
+  CREATE VIEW payouts AS
+    SELECT
+      eb.bet_id,
+      eb.better_id,
+      eb.prediction_id,
+      eb.predictor_id,
+      eb.endorsed,
+      eb.status,
+      eb.wager,
+      ep.endorsement_ratio,
+      ep.undorsement_ratio,
+      (SELECT 
+        COALESCE(
+          NULLIF(
+            FLOOR(
+              eb.wager *
+              (CASE
+                WHEN eb.status = 'successful'
+                THEN ep.endorsement_ratio
+                ELSE ep.undorsement_ratio
+              END)
+            ), 0
+          ), 1
+        )  *
+        (CASE
+          WHEN 
+            (eb.status = 'successful' AND eb.endorsed IS TRUE) OR 
+            (eb.status = 'failed' AND eb.endorsed IS FALSE)
+          THEN 1
+          ELSE -1
+        END)
+      ) as payout
+    FROM enhanced_bets eb
+    JOIN enhanced_predictions ep ON ep.prediction_id = eb.prediction_id
+    WHERE eb.status = 'successful' OR eb.status = 'failed'
+`;
+
+const CREATE_USERS_SCORE_SUMMARY = `
+  CREATE VIEW users_scores_summary AS
+    SELECT
+      users.id as better_id,
+      COALESCE(SUM(payout), 0) as points,
+      RANK () OVER (
+        ORDER BY 
+          COALESCE(SUM(payout), 0) DESC
+      )
+    FROM users
+    LEFT JOIN payouts ON payouts.better_id = users.id
+    GROUP BY users.id
 `;
 
 const GET_USER_SCORE_BY_ID = `
     SELECT
+      (SELECT row_to_json(score_sum) FROM (
+        SELECT points, rank FROM users_scores_summary us WHERE us.better_id  = $1
+      ) score_sum) as score,
       (SELECT row_to_json(pred_sum) FROM (
         SELECT successful, failed, pending, retired, rank FROM users_predictions_summary WHERE id = $1
       ) pred_sum) as predictions,
       (SELECT row_to_json(bet_sum) FROM (
         SELECT successful, failed, pending, retired, rank FROM users_bets_summary WHERE id = $1
-      ) bet_sum) as bets
+      ) bet_sum) as bets,
+      (SELECT COUNT(votes.id) FILTER (WHERE votes.user_id = $1)::INT FROM votes) as votes
 `;
 
 export default {
