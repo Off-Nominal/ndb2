a65b7f67c4b77d35628a0d31ae6e33c65057bfb8
Jake Robins <me@jakerobins.com>
2023-05-24 10:56:37 -0600

    remove console.log

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -29,8 +29,6 @@ const generate_GET_USER_SCORE_BY_ID_with_SEASON = (
   `
     : "";
 
-  console.log(generate_GET_USER_SCORE_SUMMARY_with_SEASON(seasonId));
-
   return `
     WITH 
       users_scores_summary 
