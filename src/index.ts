ec04a31f16f0a440f32a5adebae65a815cb75311
Jake Robins <me@jakerobins.com>
2024-09-07 12:50:46 -0600

    add health check

--- src/index.ts
+++ src/index.ts
@@ -15,21 +15,29 @@ app.use(express.json());
 
 // Authentication
 app.use(validateContentType);
-app.use(authenticateApplication);
+// app.use(authenticateApplication);
+
+app.get("/health", (req, res) => {
+  return res.status(200).json({ status: "healthy" });
+});
+
+const autheticatedRouter = express.Router();
+autheticatedRouter.use(authenticateApplication);
 
 // Routers
 import predictionsRouter from "./routers/predictions";
-app.use("/api/predictions", predictionsRouter);
+autheticatedRouter.use("/api/predictions", predictionsRouter);
 
 import usersRouter from "./routers/users";
-app.use("/api/users", usersRouter);
+autheticatedRouter.use("/api/users", usersRouter);
 
 import scoresRouter from "./routers/scores";
-app.use("/api/scores", scoresRouter);
+autheticatedRouter.use("/api/scores", scoresRouter);
 
 import seasonsRouter from "./routers/seasons";
+autheticatedRouter.use("/api/seasons", seasonsRouter);
 
-app.use("/api/seasons", seasonsRouter);
+app.use(autheticatedRouter);
 
 app.get("*", (req, res) => {
   return res.status(404).json({ error: "Not found" });
